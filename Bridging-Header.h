2669fb819911ff96d6e1d8c7ab4af8093b44ee4c
FrogmentCambodia <frogment.cambodia@gmail.com>
2019-04-20 16:09:21 +0100

    Initial Commit

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  FrogMallFir
+//
+//  Created by 阿瀬義弘 on 2018/05/28.
+//  Copyright © 2018年 Frogment. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_Header_h */
